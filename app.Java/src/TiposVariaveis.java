f6cf76d9fb1cdd5bb2f6bb6ae07c8d73028f07b9
Raphael <euraphaelbrandao@gmail.com>
2025-07-05 17:36:58 -0300

    Primeiro Commit

--- app.Java/src/TiposVariaveis.java
+++ app.Java/src/TiposVariaveis.java
@@ -1,7 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+
         double salarioMinimo = 2500;
 
-        }
+        String meuNome = "Raphael Brandão";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        Short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+
+
     }
-}
+
+    }
